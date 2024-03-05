c66e46724a880aa45193908d82c4c7646041837b
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-03-05 18:23:07 -0500

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  target: 'electron-renderer',
+  fs: false,
+  target: 'web',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -14,6 +15,11 @@ module.exports = {
     fallback: {
       "path": require.resolve("path-browserify"),
     },
+  },
+  browser: {
+    "fs": false,
+    "os": false,
+    "path": false,
   },
   devtool: 'source-map',
   module: {
