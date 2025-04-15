052180d3e0cc9b8eab4d3f1379acd109040697ba
Superior125 <90008321+Superior126@users.noreply.github.com>
2025-04-15 16:32:44 -0400

    Fix code scanning issues

--- src/components/main/friends_list/component.js
+++ src/components/main/friends_list/component.js
@@ -15,12 +15,12 @@ export default function FriendsList({
     function handle_friend_request_accept(data) {
       if (friendsListState !== "loading" && typeof friendsListState === "object") {
         // Create clone of friends list
-        let friends_list = [...friendsListState];
+        let friendsList = [...friendsListState];
   
         let user_found = false;
   
         // Check if user is already in list
-        friends_list.forEach((user) => {
+        friendsList.forEach((user) => {
           if (user.Username === data.detail.username) {
             user_found = true;
             console.log("found user");
@@ -28,7 +28,7 @@ export default function FriendsList({
         });
   
         if (!user_found) {
-          friends_list.push({
+          friendsList.push({
             Username: data.detail.username,
             Id: data.detail.id,
             Online: data.detail.user_online,
@@ -36,7 +36,7 @@ export default function FriendsList({
           });
   
           // Update friends list
-          setFriendsListState(friends_list);
+          setFriendsListState(friendsList);
         }
       } else if (friendsListState !== "loading" && typeof friendsListState !== "object") {
         // Set new friends list
@@ -46,16 +46,16 @@ export default function FriendsList({
   
     function handle_conversation_removal(data) {
       // Create clone of friends list
-      let friends_list = [...friendsListState];
+      let friendsList = [...friendsListState];
   
       // Keep track of conversation index
       let index = 0;
   
       // Check if conversation exists
-      friends_list.forEach((conversation) => {
+      friendsList.forEach((conversation) => {
         if (conversation.Id === data.detail.id) {
           console.log("found conversation")
-          friends_list.splice(index, 1);
+          friendsList.splice(index, 1);
   
         } else {
           index += 1;
@@ -63,7 +63,7 @@ export default function FriendsList({
       })
   
       // Update friends list
-      setFriendsListState(friends_list);
+      setFriendsListState(friendsList);
   
       // Check if removed conversation is currently selected
       if (data.detail.id === conversation_id) {
@@ -75,17 +75,17 @@ export default function FriendsList({
       console.log(data.detail.user + " is now " + data.detail.status);
   
       // Make a copy of the friends list to modify
-      let friends_list = [...friendsListState];
+      let friendsList = [...friendsListState];
   
       // Cycle through the list and update the users online status
-      friends_list.forEach(friend => {
+      friendsList.forEach(friend => {
         if (friend.Username === data.detail.user) {
           friend.Online = data.detail.status;
         }
       })
   
       // Update friends list state with new friends list
-      setFriendsListState(friends_list);
+      setFriendsListState(friendsList);
     }
   
     document.addEventListener("Friend_Request_Accept", handle_friend_request_accept);
@@ -149,15 +149,15 @@ export default function FriendsList({
       console.log(data.detail.message);
 
       // Make a copy of the friends list to modify
-      let friends_list = [...friendsListState];
+      let friendsList = [...friendsListState];
 
-      friends_list.forEach((user) => {
+      friendsList.forEach((user) => {
         if (user.Id === data.detail.conversation_id) {
           user.Last_Message = data.detail.message;
         }
       });
 
-      setFriendsListState(friends_list);
+      setFriendsListState(friendsList);
     }
 
     // Listen for message updates and update the friends list
@@ -172,7 +172,7 @@ export default function FriendsList({
   
     if (friendsListState === "loading") {
       return(
-        <div className={styles.friends_list}>
+        <div className={styles.friendsList}>
           <div className="loader">
             <div className="avatar" />
             <div className="username" />
@@ -189,14 +189,14 @@ export default function FriendsList({
       )
     } else if (friendsListState === "Error"){
       return(
-        <div className={styles.friends_list}>
+        <div className={styles.friendsList}>
           <img src={Error_Image} alt='Error' className='error_image' />
           <p>Something Went Wrong!</p>
         </div>
       )
     } else if (Array.isArray(friendsListState)) {
       return(
-          <div className={styles.friends_list}>
+          <div className={styles.friendsList}>
             {friendsListState.map(item => (
                 <Friend
                   username={item.Username}
